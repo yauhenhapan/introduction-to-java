e31adae194d00e0c6d13e54e1b798156874f6cf5
Eugene Gapanovich <evgengapanovich@gmail.com>
2021-01-07 01:46:41 +0300

    upd: added file gitignore and implemented two tasks from linear part

---
+++ Basics of software code development/src/by/gapanovich/basics/linear/Task1.java
@@ -0,0 +1,32 @@
+package by.gapanovich.basics.linear;
+
+/*
+    Task:
+    Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
+ */
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.print("Enter three double numbers\nFirst number:");
+        double firstNumber = enterDouble(System.in);
+        System.out.print("Second number:");
+        double secondNumber = enterDouble(System.in);
+        System.out.print("Third number:");
+        double thirdNumber = enterDouble(System.in);
+
+        System.out.println("The value of function = " + findValue(firstNumber, secondNumber, thirdNumber));
+    }
+
+    private static double enterDouble(InputStream input){
+        Scanner scan = new Scanner(input);
+        return scan.nextDouble();
+    }
+
+    private static double findValue(double a, double b, double c){
+        return ((a-3) * b / 2) + c;
+    }
+}
---
+++ Basics of software code development/src/by/gapanovich/basics/linear/Task2.java
@@ -0,0 +1,41 @@
+package by.gapanovich.basics.linear;
+
+/*
+    Task:
+    Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+    ( (b + sqrt(b^2 + 4*a*c)) / 2*a ) - (a^3)*c + 1/(b^2)
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter three double numbers\nFirst number:");
+        double firstNumber = enterDouble(System.in);
+        System.out.print("Second number:");
+        double secondNumber = enterDouble(System.in);
+        System.out.print("Third number:");
+        double thirdNumber = enterDouble(System.in);
+
+        System.out.println("Result:" + findValue(firstNumber, secondNumber, thirdNumber));
+    }
+
+    private static double enterDouble(InputStream input){
+        Scanner scan = new Scanner(input);
+        return scan.nextDouble();
+    }
+
+    private static double findValue(double a, double b, double c) throws IOException {
+        if(isCorrectValues(a, b, c)){
+            return  ((b + Math.sqrt(Math.pow(b,2) + 4*a*c)) / 2*a ) - Math.pow(a,3) + 1/Math.pow(b,2);
+        } else {
+            throw new IOException("Input values are not correct!");
+        }
+    }
+
+    private static boolean isCorrectValues(double a, double b, double c){
+        return a != 0 && b != 0 && (Math.pow(b,2) + 4*a*c) >= 0;
+    }
+}
