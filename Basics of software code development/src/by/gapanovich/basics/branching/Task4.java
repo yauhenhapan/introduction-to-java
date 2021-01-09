09cf349ccf40c022de4467ed80649aca99b8d15b
Eugene Gapanovich <evgengapanovich@gmail.com>
2021-01-09 10:47:33 +0300

    upd: implemented last tasks in branching part

---
+++ Basics of software code development/src/by/gapanovich/basics/branching/Task4.java
@@ -0,0 +1,38 @@
+package by.gapanovich.basics.branching;
+
+/*
+    Task:
+    Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича.
+    Определить, пройдет ли кирпич через отверстие.
+ */
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.print("A = ");
+        double A = enterValue(System.in);
+        System.out.print("B = ");
+        double B = enterValue(System.in);
+
+        System.out.print("x = ");
+        double x = enterValue(System.in);
+        System.out.print("y = ");
+        double y = enterValue(System.in);
+        System.out.print("z = ");
+        double z = enterValue(System.in);
+
+        System.out.println("Will the brick go through the hole: " + isFitBrick(A, B, x, y, z));
+    }
+
+    private static boolean isFitBrick(double A, double B, double x, double y, double z){
+        return (x <= A && y <= B) || (x <= B && y <= A) || (x <= A && z <= B) || (z <= A && x <= B) || (y <= A && z <= B) &&
+                (z <= A && y <= B);
+    }
+
+    private static double enterValue(InputStream input){
+        Scanner scan = new Scanner(input);
+        return scan.nextDouble();
+    }
+}
---
+++ Basics of software code development/src/by/gapanovich/basics/branching/Task5.java
@@ -0,0 +1,32 @@
+package by.gapanovich.basics.branching;
+
+/*
+    Task:
+    Вычислить значение функции:
+    F(x) = x^2 - 3x + 9 , if x <= 3 AND 1/(x^3 + 6) , if x > 3
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("Enter value");
+        System.out.print("x = ");
+        double x = enterValue(System.in);
+        System.out.print("The result of function = " + findValueFunc(x));
+    }
+
+    private static double findValueFunc(double x){
+        if (x <= 3) {
+            return Math.pow(x, 2) - 3 * x + 9;
+        }
+        return Math.pow(Math.pow(x, 3) + 6, -1);
+    }
+
+    private static double enterValue(InputStream input){
+        Scanner scan = new Scanner(input);
+        return scan.nextDouble();
+    }
+}
